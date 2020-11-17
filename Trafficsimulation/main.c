f6c574099902d1a30624f7e98cba53677fc6fea8
Lasse Falden <lfalde20@student.aau.dk>
2020-11-17 10:50:13 +0100

    Update main.c
    Virker det?

--- Trafficsimulation/main.c
+++ Trafficsimulation/main.c
@@ -4,6 +4,7 @@ int main() {
     printf("Hello, World!\n");
     int testerino = 123;
     printf("testeteststetst\n");
+    printf("Hallo kan Lasse få det til at virke test123Kappaxd\n");
     /* Tilføjede en kommentar på linje 7 */
     return 0;
     
