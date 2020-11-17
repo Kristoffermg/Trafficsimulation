5504c79518dbf82c0e8b99f43f96a83651e95ee1
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2020-11-17 10:26:28 +0100

    taesasd
    asdasdasdsad

--- Trafficsimulation/cmake-build-debug/CMakeFiles/Trafficsimulation.dir/DependInfo.cmake
+++ Trafficsimulation/cmake-build-debug/CMakeFiles/Trafficsimulation.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Kristoffer/Dropbox/Auni/Trafficsimulation/main.c" "C:/Users/Kristoffer/Dropbox/Auni/Trafficsimulation/cmake-build-debug/CMakeFiles/Trafficsimulation.dir/main.c.obj"
+  "C:/Users/Kristoffer/Documents/GitHub/Trafficsimulation/Trafficsimulation/main.c" "C:/Users/Kristoffer/Documents/GitHub/Trafficsimulation/Trafficsimulation/cmake-build-debug/CMakeFiles/Trafficsimulation.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- Trafficsimulation/cmake-build-debug/cmake_install.cmake
+++ Trafficsimulation/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Kristoffer/Dropbox/Auni/Trafficsimulation
+# Install script for directory: C:/Users/Kristoffer/Documents/GitHub/Trafficsimulation/Trafficsimulation
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Kristoffer/Dropbox/Auni/Trafficsimulation/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Kristoffer/Documents/GitHub/Trafficsimulation/Trafficsimulation/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
