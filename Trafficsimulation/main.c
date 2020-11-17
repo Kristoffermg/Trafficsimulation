e248bf2521edfacfb261b88c822bfb7e55cf6db9
Jakob Tophotl Jensen <jtje20@student.aau.dk>
2020-11-17 12:31:25 +0100

    Tilføjede endnu en printf
    Linje 9:
    printf("%d ", testerino);

--- Trafficsimulation/main.c
+++ Trafficsimulation/main.c
@@ -6,6 +6,7 @@ int main() {
     printf("testeteststetst\n");
     printf("Hallo kan Lasse få det til at virke test123Kappaxd\n");
     printf("Gjorde han det?\n");
+    printf("%d ", testerino);
     /* Tilføjede en kommentar på linje 7 */
     return 0;
     
