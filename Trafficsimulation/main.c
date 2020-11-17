571320e22fe1e5e5a8df4d4a16d958b3059851f8
Patrick Bertelsen <pberte20@users.noreply.github.com>
2020-11-17 11:03:12 +0100

    Lavede gjorde han det linje

--- Trafficsimulation/main.c
+++ Trafficsimulation/main.c
@@ -5,6 +5,7 @@ int main() {
     int testerino = 123;
     printf("testeteststetst\n");
     printf("Hallo kan Lasse få det til at virke test123Kappaxd\n");
+    printf("Gjorde han det?\n");
     /* Tilføjede en kommentar på linje 7 */
     return 0;
     
