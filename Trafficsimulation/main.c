b9e9a8c69ef4a24f3ed3a2d911fc1f6acb7d9052
MartinSvanholm <msvanh20@student.aau.dk>
2020-12-02 12:48:51 +0100

    Chance to struct pointers

--- Trafficsimulation/main.c
+++ Trafficsimulation/main.c
@@ -27,25 +27,26 @@ typedef struct Car_Route Car_Route;
 
 enum direction { North, South, East, West };
 
-void get_route(struct Car_Route cr);
+void get_route(Car_Route *cr);
 
 int main() {
     Car_Route cr;
-    get_route(cr);
+    get_route(&cr);
 
     return EXIT_SUCCESS;
 }
 
-void get_route(struct Car_Route cr) {
+void get_route(Car_Route *cr) {
     char *direction;
     int i;
 
-    cr.start_position = 1;
-    cr.intersections[0] = East; cr.intersections[1] = East; cr.intersections[2] = East; cr.intersections[3] = East; cr.intersections[4] = East;
+    cr->start_position = 1;
 
-    printf("Starting position = %d\n", cr.start_position);
+    cr->intersections[0] = East; cr->intersections[1] = East; cr->intersections[2] = East; cr->intersections[3] = East; cr->intersections[4] = East;
+
+    printf("Starting position = %d\n", cr->start_position);
     for(i = 0; i < 5; i++) {
-        switch (cr.intersections[i]) {
+        switch (cr->intersections[i]) {
             case 0: direction = "North"; break;
             case 1: direction = "South"; break;
             case 2: direction = "East"; break;
