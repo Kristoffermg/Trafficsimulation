b0801b4e969569847a387f350d1f09249475043e
Kristoffermg <kristoffermoeller151@gmail.com>
2020-12-02 13:00:06 +0100

    Oprydning
    - Indrykning fikset
    - Slettet en return-værdi som ikke burde være der

--- Trafficsimulation/main.c
+++ Trafficsimulation/main.c
@@ -40,40 +40,33 @@ int main() {
     Car car;
     Car car1;
     car1 = Create_Car(car);
-    while (Run_Car(&car1,time) != 1)
-    {
+    while (Run_Car(&car1,time) != 1) {
         time++;
     }
     
     return EXIT_SUCCESS;
 }
 
-Car Create_Car(Car car)
-{
-car.current_position = 0;
-car.start_time = 0;
-car.route = 5;
-return car;
+Car Create_Car(Car car) {
+    car.current_position = 0;
+    car.start_time = 0;
+    car.route = 5;
+    return car;
 }
 
-int Run_Car(Car *car, double time)
-{
-    if (car->current_position < 800)
-    {
-    car->current_speed = MAX_SPEED;
-    car->current_position = car->current_position + car->current_speed;
-    printf("position: %lf tid: %lf\n",car->current_position,time);
-    return 0;
+int Run_Car(Car *car, double time) {
+    if (car->current_position < 800) {
+        car->current_speed = MAX_SPEED;
+        car->current_position = car->current_position + car->current_speed;
+        printf("position: %lf tid: %lf\n",car->current_position,time);
+        return 0;
     }
-    else if (car->current_position > 800)
-    {
-    printf("bil færdig tid: %lf",time - car->start_time);
-    return 1;
+    else if (car->current_position > 800) {
+        printf("bil færdig tid: %lf",time - car->start_time);
+        return 1;
     }
-}
+
     get_route(&cr);
-
-    return EXIT_SUCCESS;
 }
 
 void get_route(Car_Route *cr) {
