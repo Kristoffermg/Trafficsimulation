702577769f41ec8543fda5a67ada37b17fbe2e30
JakobTopholt <jtje20@student.aau.dk>
2020-12-10 10:07:12 +0100

    Oversatt til engelsk
    ^^

--- Trafficsimulation/main.c
+++ Trafficsimulation/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_SPEED 14 /* 50km/t i m/s */
-#define CAR_LENGTH 4 /* meter */
+#define MAX_SPEED 14 /* 50km/t in m/s */
+#define CAR_LENGTH 4 /* meters */
 #define MAX_CARS 100
 #define MAX_ROUTES 20
 #define MAX_TIME_VALUES 5000
@@ -13,12 +13,12 @@ typedef struct Cars {
     double start_time;
     int route;
     float current_position;
-    int driving_direction; /* bruger enum directions værdier */
+    int driving_direction; /* Uses enum direction values */
 } Cars;
 
 struct Intersection {
     int is_traffic_light;
-    int road_connections; /* 0: nordpå, 1: sydpå, 2: begge */
+    int road_connections; /* 0: northwards, 1: southwards, 2: both */
 };
 
 typedef struct Car_Route {
@@ -51,7 +51,7 @@ int main() {
     while (Run_Car(&car[1], time, cr, All_Times) != 1) {
         time++;
     }
-    printf("Test af return_time: skal være ligemed n's bils tid: %d\n ",All_Times[0]);
+    printf("Test of return_time: must be equal to n's car's time: %d\n ",All_Times[0]);
     return EXIT_SUCCESS;
 }
 
@@ -108,12 +108,12 @@ int car_turning(Cars *car, double time, Car_Route *cr, int *All_Times){
     }
     else if(car->current_position > 200 && car->current_position < 225) {
         car->driving_direction = driving_direction(*car, *cr, 1);
-        printf("bil drejer tid: %lf", time - car->start_time);
+        printf("Car turns time: %lf", time - car->start_time);
         Return_Time(time - car->start_time, All_Times);
         return 1;
     }
     else if (car->current_position > 800) {
-        printf("bil færdig tid: %lf\n", time - car->start_time);
+        printf("Car finish time: %lf\n", time - car->start_time);
         Return_Time(time - car->start_time, All_Times);
     }
     else
@@ -122,7 +122,7 @@ int car_turning(Cars *car, double time, Car_Route *cr, int *All_Times){
 
 void Return_Time(int Car_Time, int *All_Times)
 {
-    /*Static siden counteren skal tælle 1 op hver gang funktionen kaldes */
+    /*Static since the counter must count up 1 every time the function is called */
     static int Car_Count = 0;
     All_Times[Car_Count] = Car_Time;
     Car_Count++;
